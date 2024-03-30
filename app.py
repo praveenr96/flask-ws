0d61b9ac2c6e71379d0e046dcb1ee796de7f087e
praveenr96 <praveenrk189@gmail.com>
2024-03-30 08:29:35 +0530

    Inital Commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+from flask_sock import Sock
+
+app = Flask(__name__)
+sock = Sock(app)
+
+
+@app.route("/webhooks/answer")
+def answer_call():
+    ncco = [
+        {
+            "action": "talk",
+            "text": "We will now connect you to the echo server, wait a moment then start speaking.",
+        },
+        {
+            "action": "connect",
+            "from": "Vonage",
+            "endpoint": [
+                {
+                    "type": "websocket",
+                    "uri": f"wss://{request.host}/socket",
+                    "content-type": "audio/l16;rate=16000",
+                }
+            ],
+        },
+    ]
+
+    return jsonify(ncco)
